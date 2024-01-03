55a8ca12f9ccc5c44d6bf024b5164238a46a6f57
AfolarinOyelekeRumbl <afolarin.oyeleke@rumbl.co.uk>
2024-01-03 12:49:34 +0000

    update db settings

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,9 @@
 import pg from 'pg';
 
 export const pool = new pg.Pool({
-	user:
-		process.env.NODE_ENV === 'development'
-			? 'postgres'
-			: 'symptopm_checker_user',
-	host:
-		process.env.NODE_ENV === 'development'
-			? 'localhost'
-			: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	host: process.env.DATABASE_HOST,
 	port: 5432,
 	database: 'symptopm_checker',
-	password:
-		process.env.NODE_ENV === 'development' ? '' : process.env.DATABASE_PASSWORD,
+	password: process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : '',
 });
